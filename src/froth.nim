5d9b8304e2bd24b10b99cbd60bac777343f2646c
metagn <metagngn@gmail.com>
2026-04-10 20:22:48 +0300

    add example to readme, broader warning

--- src/froth.nim
+++ src/froth.nim
@@ -1,4 +1,7 @@
 import froth/[lowerbits, lowerbyte, lowershort, upperbits, upperbyte, uppershort]
 export lowerbits, lowerbyte, lowershort, upperbits, upperbyte, uppershort
 
+import froth/condensate
+export condensate
+
 # missing nanboxes
